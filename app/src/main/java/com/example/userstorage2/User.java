ab0dc9f6e1e917a4435cc5539c23044244f3ab90
Roope Myller <roope.myller@gmail.com>
2023-03-28 22:04:14 +0300

    userStorage2 with saving and loading users

---
+++ app/src/main/java/com/example/userstorage2/User.java
@@ -0,0 +1,41 @@
+package com.example.userstorage2;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class User implements Serializable {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String degreeProgram;
+
+    private ArrayList<String> completedDegrees;
+
+    public User(String firstName, String lastName, String email, String degreeProgram) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.degreeProgram = degreeProgram;
+        this.completedDegrees = new ArrayList<>();
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getDegreeProgram() {
+        return degreeProgram;
+    }
+
+    public ArrayList<String> getCompletedDegrees() {
+        return completedDegrees;
+    }
+}
---
+++ app/src/main/java/com/example/userstorage2/UserStorage.java
@@ -0,0 +1,58 @@
+package com.example.userstorage2;
+
+import android.content.Context;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+
+public class UserStorage {
+    private ArrayList<User> users = new ArrayList<>();
+    private static UserStorage storage = null;
+
+    private UserStorage(){
+    }
+
+    public static UserStorage getInstance(){
+        if(storage == null){
+            storage = new UserStorage();
+        }
+        return storage;
+    }
+
+    public ArrayList<User> getUsers(){
+        return users;
+    }
+
+    public void addUser(User user){
+        users.add(user);
+        return;
+    }
+
+    public void saveUsers(Context context){
+        try{
+            ObjectOutputStream userWriter = new ObjectOutputStream(context.openFileOutput("users.data", Context.MODE_PRIVATE));
+            userWriter.writeObject(users);
+            userWriter.close();
+        } catch (IOException e) {
+            System.out.println("Käyttäjien tallentaminen epäonnistui.");
+        }
+    }
+
+    public void loadUsers(Context context){
+        try{
+            ObjectInputStream userReader = new ObjectInputStream(context.openFileInput("users.data"));
+            users = (ArrayList<User>) userReader.readObject();
+            userReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Käyttäjien lukeminen epäonnistui.");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Käyttäjien lukeminen epäonnistui.");
+        } catch (IOException e) {
+            System.out.println("Käyttäjien lukeminen epäonnistui.");
+        }
+    }
+
+}
